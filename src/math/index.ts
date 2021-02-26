a6c4fa6e1049658f8dd8772af4fefb74bb37a1f1
Erko Bridee <erko.bridee@gmail.com>
2021-02-26 17:10:31 +0100

    update(math): add default export

--- src/math/index.ts
+++ src/math/index.ts
@@ -2,3 +2,8 @@ export const add = (...args: number[]) =>
   args.reduce((acc, value) => acc + value, 0);
 
 export const subtract = (a: number, b: number) => a - b;
+
+export default {
+  add,
+  subtract,
+};
