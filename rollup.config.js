df77e05a2144253ce02f8c361a6c569d22d5fd31
Erko Bridee <erko.bridee@gmail.com>
2021-03-01 14:49:43 +0100

    feat(project): add lint-staged

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 import kebabCase from 'lodash.kebabcase';
@@ -52,6 +53,8 @@ const {
 const libraryFileName = kebabCase(libraryName);
 
 const tsPluginConfig = {
+  clean: true,
+  rollupCommonJSResolveHack: true,
   useTsconfigDeclarationDir: true,
   tsconfigOverride: {
     compilerOptions: {
@@ -62,11 +65,17 @@ const tsPluginConfig = {
   },
 };
 
+const babelConfig = {
+  exclude: 'node_modules/**',
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+};
+
 const rollupCommonPlugins = [
+  typescript(tsPluginConfig),
+  babel(babelConfig),
   resolve(),
   commonjs(),
   json(),
-  typescript(tsPluginConfig),
 ];
 
 const bundleConfig = {
